8c0dcc7dd0c495e5e894427c3fbb4473baf58a48
LordGee <gordon.johnson@mychaos.co.uk>
2016-05-09 14:31:31 +0100

    Added cause and effect prototype VS Form App
    This document will be used to create a prototype that demonstrates the
    collision of two round objects and and the effects such as direction and
    velocity after.

---
+++ CauseAndEffect/CauseAndEffect/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CauseAndEffect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // This is a prototype to test for collision between two elipse 
+        // This will also demonstrate to collision effect to move both objects
+        // in an appropriate direction and speed.
+    }
+}
