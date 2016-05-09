214d616f66ec8480ae97d36331fe17e67b82fed2
LordGee <gordon.johnson@mychaos.co.uk>
2016-05-09 16:59:16 +0100

    C&E Prototype - Added two shapes
    One moves towards the other, detection is made and stops the timer

--- CauseAndEffect/CauseAndEffect/Form1.cs
+++ CauseAndEffect/CauseAndEffect/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,56 @@ namespace CauseAndEffect
 {
     public partial class Form1 : Form
     {
+        // This is a prototype to test for collision between two elipse 
+        // This will also demonstrate to collision effect to move both objects
+        // in an appropriate direction and speed.
         public Form1()
         {
             InitializeComponent();
         }
-        // This is a prototype to test for collision between two elipse 
-        // This will also demonstrate to collision effect to move both objects
-        // in an appropriate direction and speed.
+        // public variables
+        public Rectangle bowl1, bowl2;
+        public Point bowlA = new Point(0, 100);
+        public Point bowlB = new Point(300,100);
+        public int bowlAV = 600;
+        public int bowlW = 75;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tmrAnimate.Interval = 30;
+            tmrAnimate.Start();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // when the formloads draw two elipse
+            e.Graphics.FillEllipse(Brushes.Green, bowl1);
+            e.Graphics.FillEllipse(Brushes.Blue, bowl2);
+        }
+
+        private void tmrAnimate_Tick(object sender, EventArgs e)
+        {
+            
+            if (bowlA.X < bowlAV)
+            {
+                bowlA.X = bowlA.X + 2;
+                bowlAV--;
+                
+            }
+            else
+            {
+                tmrAnimate.Stop();
+            }
+            bowl1 = new Rectangle(bowlA.X, bowlA.Y, bowlW, bowlW);
+            bowl2 = new Rectangle(bowlB.X, bowlB.Y, bowlW, bowlW);
+            if (bowl1.IntersectsWith(bowl2))
+            {
+                tmrAnimate.Stop();
+            }
+            else
+            {
+                Refresh();
+            }
+            
+        }
     }
 }
