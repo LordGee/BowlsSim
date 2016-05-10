b51ca5bcc1b3f48c3984c1012e836245ea4ac73f
LordGee <gordon.johnson@mychaos.co.uk>
2016-05-10 23:23:37 +0100

    Comments
    Added comments to the BowlsSimulator project

--- BowlsSimulator/BowlsSimulator/Form1.cs
+++ BowlsSimulator/BowlsSimulator/Form1.cs
@@ -18,28 +18,28 @@ namespace BowlsSimulator
             InitializeComponent();
         }
 
-        //Public Variables Go Here
+        // Public Variables Go Here
         public int fs = 36; // Define the size of the font used
         public int screenWidth, screenHeight; // Varable to hold the size of the form maximised on load
         public int bannerHeight, gameHeight; // Variables to gold the height of the game area and the heaight for the header and footers.
-        public Font ff;
+        public Font ff; // Font variable that holds the main font that will be used throughout the application
 
         private void frmMainGame_Paint(object sender, PaintEventArgs e)
         {
-            //Font ff = new Font("resources/Comfortaa-Regular.ttf", fs, FontStyle.Bold); // defines the font style for the graphic text used
+            // Font ff = new Font("resources/Comfortaa-Regular.ttf", fs, FontStyle.Bold); // defines the font style for the graphic text used // No longer needed as it's now in its own function
             gameHeight = (screenWidth / 7) + 200; // calculation to work out the game area
             bannerHeight = (screenHeight - gameHeight) / 2; // the height of the header and footer banners
             e.Graphics.FillRectangle(Brushes.LightGreen, 0, 0, screenWidth, bannerHeight); // draw the header banner
             e.Graphics.FillRectangle(Brushes.DarkOliveGreen, 0, bannerHeight + gameHeight, screenWidth, bannerHeight); // draw the footer banner
             e.Graphics.DrawString("Exit Game", ff, Brushes.DarkRed, new Point(screenWidth - 300, screenHeight - bannerHeight / 2)); // draw the exit button
-            e.Graphics.DrawString("Option ", ff, Brushes.DarkBlue, new Point(50, screenHeight - bannerHeight / 2)); // draw the options button
+            e.Graphics.DrawString("Options", ff, Brushes.DarkBlue, new Point(50, screenHeight - bannerHeight / 2)); // draw the options button
            
         }
 
         private void frmMainGame_Load(object sender, EventArgs e)
         {
             screenSize(); // executes function to find out the form width and height at launch
-            customFont();
+            customFont(); // executes function that adds a custom font family to be used regardless of whether the user has it installed or not
         }
 
         public void screenSize()
@@ -50,9 +50,9 @@ namespace BowlsSimulator
 
         public void customFont()
         {
-            PrivateFontCollection pfc = new PrivateFontCollection();
-            pfc.AddFontFile("resources/Comfortaa-bold.ttf");
-            ff = new Font(pfc.Families[0], fs, FontStyle.Bold);
+            PrivateFontCollection pfc = new PrivateFontCollection(); // Creates a new instance of a Private Font Collection
+            pfc.AddFontFile("resources/Comfortaa-bold.ttf"); // Adds a font held in the application resources into the collection pfc
+            ff = new Font(pfc.Families[0], fs, FontStyle.Bold); // defines the variable ff with the font family, size and style
         }
     }
 }
