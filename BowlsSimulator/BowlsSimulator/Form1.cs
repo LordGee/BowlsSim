7918ca1a3e161724d855be43311f174787ef15d0
jeremiastiagodembo <kc37046@student.kingston-college.ac.uk>
2016-05-11 13:48:07 +0100

    Generic layout  is completed
    All object in correct place,  added 100 pixel to the game height. We may
    need to change  some objects colour.

--- BowlsSimulator/BowlsSimulator/Form1.cs
+++ BowlsSimulator/BowlsSimulator/Form1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Drawing.Text;
+using System.Drawing.Text; //Needed to add a Private Font Collection class
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,17 +23,20 @@ namespace BowlsSimulator
         public int screenWidth, screenHeight; // Varable to hold the size of the form maximised on load
         public int bannerHeight, gameHeight; // Variables to gold the height of the game area and the heaight for the header and footers.
         public Font ff; // Font variable that holds the main font that will be used throughout the application
+        public int ditchW=30;// this will be the value of the width for the ditch
 
         private void frmMainGame_Paint(object sender, PaintEventArgs e)
         {
             // Font ff = new Font("resources/Comfortaa-Regular.ttf", fs, FontStyle.Bold); // defines the font style for the graphic text used // No longer needed as it's now in its own function
-            gameHeight = (screenWidth / 7) + 200; // calculation to work out the game area
+            gameHeight = (screenWidth / 7) + 200; // calculation to work out the game area // increased from the design to 200 from 100 to make the game area bigger then the banners
             bannerHeight = (screenHeight - gameHeight) / 2; // the height of the header and footer banners
             e.Graphics.FillRectangle(Brushes.LightGreen, 0, 0, screenWidth, bannerHeight); // draw the header banner
-            e.Graphics.FillRectangle(Brushes.DarkOliveGreen, 0, bannerHeight + gameHeight, screenWidth, bannerHeight); // draw the footer banner
-            e.Graphics.DrawString("Exit Game", ff, Brushes.DarkRed, new Point(screenWidth - 300, screenHeight - bannerHeight / 2)); // draw the exit button
-            e.Graphics.DrawString("Options", ff, Brushes.DarkBlue, new Point(50, screenHeight - bannerHeight / 2)); // draw the options button
-           
+            e.Graphics.FillRectangle(Brushes.LightGreen, 0, bannerHeight + gameHeight, screenWidth, bannerHeight); // draw the footer banner
+            e.Graphics.DrawString("Exit Game", ff, Brushes.DarkRed, new Point(screenWidth - 300, (screenHeight - bannerHeight / 2) -(fs / 2))); // draw the exit button
+            e.Graphics.DrawString("Options", ff, Brushes.DarkBlue, new Point(50, (screenHeight - bannerHeight / 2) - (fs / 2))); // draw the options button
+            e.Graphics.FillRectangle(Brushes.Brown, 0,bannerHeight, ditchW,gameHeight); // draw the left ditch
+            e.Graphics.FillRectangle(Brushes.Brown, screenWidth - ditchW, bannerHeight, ditchW, gameHeight); // draw the right ditch
+            e.Graphics.FillRectangle(Brushes.PeachPuff, 0, bannerHeight + gameHeight, screenWidth, 30); // draw bar
         }
 
         private void frmMainGame_Load(object sender, EventArgs e)
