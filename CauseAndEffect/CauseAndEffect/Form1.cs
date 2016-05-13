3c1115d2b9df99b50ee92bccd4ea0c9cac8d673d
LordGee <KC42391@kcnet.kingston-college.ac.uk>
2016-05-13 13:38:21 +0100

    C&E Prototype - Added More Maths
    To early to explain and untested

--- CauseAndEffect/CauseAndEffect/Form1.cs
+++ CauseAndEffect/CauseAndEffect/Form1.cs
@@ -42,20 +42,29 @@ namespace CauseAndEffect
             e.Graphics.FillEllipse(Brushes.Green, bowl1);
             e.Graphics.FillEllipse(Brushes.Blue, bowl2);
         }
-
+        public bool coll = false;
         private void tmrAnimate_Tick(object sender, EventArgs e)
         {
             
-            if (bowlA.X < bowlAV)
-            {
-                bowlA.X = bowlA.X + 2;
-                bowlAV--;
-                
-            }
-            else
-            {
-                tmrAnimate.Stop();
-            }
+                if (bowlA.X < bowlAV)
+                {
+                    if (!coll)
+                    {
+                        bowlA.X = bowlA.X + 2;
+                        bowlAV--;    
+                    }
+                    else
+                    {
+                        newDirect(bowl1.X, bowl1.Y, bowl2.X, bowl2.Y);
+                        bowlAV--;
+                    }
+
+                }
+                else
+                {
+                    tmrAnimate.Stop();
+                }
+            
             bowl1 = new Rectangle(bowlA.X, bowlA.Y, bowlW, bowlW);
             bowl2 = new Rectangle(bowlB.X, bowlB.Y, bowlW, bowlW);
             //if (bowl1.IntersectsWith(bowl2))
@@ -64,7 +73,7 @@ namespace CauseAndEffect
             //}
             if (circleCollide(bowl1.X, bowl1.Y, bowl2.X, bowl2.Y, bowlW /2, bowlW / 2))
             {
-                tmrAnimate.Stop();
+                coll = true;
             }
             else
             {
@@ -72,13 +81,22 @@ namespace CauseAndEffect
             }
             
         }
+        public int newY;
         public bool circleCollide(int _X1, int _Y1, int _X2, int _Y2, int _r1, int _r2)
         {
             return (Math.Sqrt(Math.Pow(Math.Abs(_X1 - _X2), 2) + Math.Pow(Math.Abs(_Y1 - _Y2), 2)) <= (_r1 + _r2));
         }
-        //private bool innerCircle(int x1, int y1)
-        //{  // determine if the cursor is within the circle
-        //    // return (Math.Pow(x1 - (disc.x + disc.r), 2) + Math.Pow(y1 - (disc.y + disc.r), 2) < Math.Pow(disc.r, 2));
-        //}
+        public void newDirect(int _X1, int _Y1, int _X2, int _Y2)
+        {
+            double _A = Math.Abs(_X1 - _X2);
+            double _O = Math.Abs(_Y1 - _Y2);
+            double _H = Math.Sqrt(Math.Pow((_A), 2)) + Math.Sqrt(Math.Pow((_O), 2));
+            double _LX = Math.Abs(_X1 - _X2) / 2;
+            double _LY = Math.Abs(_Y1 - _Y2) / 2;
+            double _toa = Math.Abs(_O / _A);
+            double _deg = Math.Atan(_toa) * 180 / Math.PI;
+            _deg = Math.Abs(_deg + 90 - 100);
+        }
+        
     }
 }
