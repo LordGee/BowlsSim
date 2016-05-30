5ccad15e6677465c4641735411fa9aaeeb6b5807
jeremiastiagodembo <kc37046@student.kingston-college.ac.uk>
2016-05-30 12:31:19 +0100

    Comment two player game
    We  going to look at this later, once the option menu needs to be sort
    out first

--- BowlsSimulator/BowlsSimulator/Form1.cs
+++ BowlsSimulator/BowlsSimulator/Form1.cs
@@ -50,7 +50,7 @@ namespace BowlsSimulator
         bool testExitColour;
         bool testOptionColour;
         Brush twoplayerColour = Brushes.DarkGreen;
-
+        Brush pickupColour;
         class theBowls // The main dynamic class
         {
             public int play;
@@ -76,7 +76,7 @@ namespace BowlsSimulator
             g.FillRectangle(Brushes.PeachPuff, 0, bannerHeight + gameHeight, screenWidth, 30); // draw bar
             g.DrawString("Exit Game", ff, exitColour, new Point((screenWidth - 300), (screenHeight - bannerHeight / 2) - (fs / 2))); // draw the exit button
             g.DrawString("Options", ff, optionColour, new Point(50, (screenHeight - (bannerHeight / 2)) - (fs / 2))); // draw the options button
-            g.DrawString("Two Player Game", ff, twoplayerColour, new Point((screenWidth - 900), (screenHeight / 2) - (fs / 2))); //  draw option menu button
+            //g.DrawString("Two Player Game", ff, twoplayerColour, new Point((screenWidth - 900), (screenHeight / 2) - (fs / 2))); //  draw option menu button
             g.FillRegion(Brushes.Transparent, exitButton);
             g.FillRegion(Brushes.Transparent, optionsButtons);
             g.FillRegion(Brushes.White, oMat);
@@ -176,7 +176,7 @@ namespace BowlsSimulator
             }
             else if (e.Button == MouseButtons.Left && optionsButtons.IsVisible(e.Location))
             {
-                
+               Application.CommonAppDataPath 
             }
         }
 
